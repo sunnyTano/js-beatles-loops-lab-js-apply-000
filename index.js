9517803c7760b566f3f96665e7a91e8f95b00062
Susan Tan <susan.tan198@gmail.com>
2019-02-01 19:48:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-
+function theBeatlesPlay(musicians, instruments){
+  var newArr = [];
+  for(var i = 0; i<musicians.length; i++){
+    newArr.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return newArr;
+}
 
 /*
 describe('Beatles Loops', function() {
