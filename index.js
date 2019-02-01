9b7e411b5479d106a2e6056dd678ee8d6a528f7d
Susan Tan <susan.tan198@gmail.com>
2019-02-01 19:51:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ var facts = ["He was the last Beatle to learn to drive",
              "He was never a vegetarian",
              "He was a choir boy and boy scout",
              "He hated the sound of his own voice"]
-             
 function johnLennonFacts(facts){
   var newArr = [];
   for(var i =0; i<facts.length; i++){
@@ -20,6 +19,9 @@ function johnLennonFacts(facts){
   return newArr;
 }
 
+function iLoveTheBeatles(num){
+  
+}
 
 /*
   describe('johnLennonFacts', function(){
